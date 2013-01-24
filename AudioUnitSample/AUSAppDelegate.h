03582f44cd0ed27a46db617b08dfdcff335072dd
Warren Moore <wm@warrenmoore.net>
2013-01-23 21:33:41 -0500

    Initial Commit

---
+++ AudioUnitSample/AUSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AUSAppDelegate.h
+//  AudioUnitSample
+//
+//  Created by Warren Moore on 1/23/13.
+//  Copyright (c) 2013 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AUSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioUnitSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioUnitSample
+//
+//  Created by Warren Moore on 1/23/13.
+//  Copyright (c) 2013 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AUSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AUSAppDelegate class]));
+	}
+}
